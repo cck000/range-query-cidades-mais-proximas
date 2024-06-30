8611e955ad7d882fa53e526ef4631e37a2cf44b2
cck <santosguilherme783@gmail.com>
2024-06-30 17:24:46 -0400

    tirando lixo

--- build/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/programcao/cpluplus/trabed2/include/cjson/fuzzing
-# Build directory: D:/programcao/cpluplus/trabed2/build/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
